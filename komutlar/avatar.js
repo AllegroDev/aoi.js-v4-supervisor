aeaf783af6a3e217cf607bcf1cbb4b3ed3b94104
Allegro <98599309+AllegroDev@users.noreply.github.com>
2022-02-06 15:16:24 +0300

    Create avatar.js

---
+++ komutlar/avatar.js
@@ -0,0 +1,10 @@
+module.exports = {
+name:"avatar",
+aliases:["pp","av"],
+code:`
+$description[$author[$username[$mentioned[1;yes]] Kişisinin Avatarı]
+$image[$userAvatar[$mentioned[1;yes]]]
+$footer[$username tarafından istendi]
+$color[#ffffff]]
+`
+}  
